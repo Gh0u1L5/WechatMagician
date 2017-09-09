91415030f182d507e79234fba91329085bd7f980
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-09-08 19:14:50 -0700

    Find recallClass in runtime
    Instead of manually reverse engineering Wechat.apk, we parse the apk at
    runtime for better compatibility.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/WechatVersion.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/WechatVersion.kt
@@ -1,33 +1,74 @@
 package com.gh0u1l5.wechatmagician.xposed
 
 import com.gh0u1l5.wechatmagician.util.Version
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedHelpers.ClassNotFoundError
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+import net.dongliu.apk.parser.ApkFile
+import net.dongliu.apk.parser.bean.DexClass
 
-class WechatVersion(pkgName: String, versionStr: String) {
+class WechatVersion(param: XC_LoadPackage.LoadPackageParam) {
 
-    private var packageName = ""
-    private var packageNameBase = ""
     var recallClass = ""
     var recallMethod = ""
     var SQLiteDatabaseClass = ""
 
     init {
-        packageName = pkgName
-        packageNameBase = packageName.substring(0, packageName.lastIndexOf("."))
+        val apkFile = ApkFile(param.appInfo.sourceDir)
+        val version = Version(apkFile.apkMeta.versionName)
 
-        val version = Version(versionStr)
         SQLiteDatabaseClass = when {
-            version >= Version("6.5.8") -> "$packageNameBase.wcdb.database.SQLiteDatabase"
-            version >= Version("6.5.3") -> "$packageNameBase.mmdb.database.SQLiteDatabase"
+            version >= Version("6.5.8") -> "com.tencent.wcdb.database.SQLiteDatabase"
+            version >= Version("6.5.3") -> "com.tencent.mmdb.database.SQLiteDatabase"
             else -> throw Error("unsupported version")
         }
 
-        recallMethod = "q"
-        recallClass = when {
-            version >= Version("6.5.13") -> "$packageName.sdk.platformtools.bg"
-            version >= Version("6.5.10") -> "$packageName.sdk.platformtools.bh"
-            version >= Version("6.5.4")  -> "$packageName.sdk.platformtools.bg"
-            version == Version("6.5.3")  -> "$packageName.sdk.platformtools.bf"
+        recallMethod = when {
+            version >= Version("6.5.3") -> "q"
             else -> throw Error("unsupported version")
         }
+        recallClass = findClassWithMethod(
+                param.classLoader,
+                findClassesFromPackage(apkFile,"com.tencent.mm.sdk.platformtools"),
+                Map::class.java, recallMethod, String::class.java, String::class.java
+        )
+    }
+
+    // getClassName parses the standard class name of the given DexClass
+    private fun getClassName(clazz: DexClass): String {
+        return clazz.classType
+                .replace('/', '.') // replace delimiters
+                .drop(1) // drop leading 'L'
+                .dropLast(1) //drop trailing ';'
+    }
+
+    // findClassesFromPackage returns a list of all the classes contained in the given package
+    private fun findClassesFromPackage(apkFile: ApkFile, packageName: String): List<DexClass> {
+        return apkFile.dexClasses.filter { it.packageName == packageName }
+    }
+
+    // findClassWithMethod finds the class that have the given method from a list of classes.
+    private fun findClassWithMethod(
+            classLoader: ClassLoader, classes: List<DexClass>,
+            returnType: Class<*>, methodName: String, vararg parameterTypes: Class<*>): String {
+        for (clazz in classes) {
+            try {
+                val className = getClassName(clazz)
+                val method = XposedHelpers.findMethodExact(
+                        className,
+                        classLoader,
+                        methodName,
+                        *parameterTypes
+                )
+                if (method.returnType == returnType) {
+                    return className
+                }
+            } catch (_: ClassNotFoundError) {
+                continue
+            } catch (_: NoSuchMethodError) {
+                continue
+            }
+        }
+        return ""
     }
 }
--- src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/XpWechat.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/XpWechat.kt
@@ -1,11 +1,9 @@
 package com.gh0u1l5.wechatmagician.xposed
 
-import android.content.Context
 import android.content.res.XModuleResources
 import android.util.SparseArray
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.IXposedHookZygoteInit
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class XpWechat : IXposedHookZygoteInit, IXposedHookLoadPackage {
@@ -23,15 +21,10 @@ class XpWechat : IXposedHookZygoteInit, IXposedHookLoadPackage {
     }
 
     override fun handleLoadPackage(param: XC_LoadPackage.LoadPackageParam) {
-        val pkgName = param.packageName
-        if (pkgName != "com.tencent.mm") return
-
-        val activityThread = XposedHelpers.findClass("android.app.ActivityThread", null)
-        val currentThread = XposedHelpers.callStaticMethod(activityThread, "currentActivityThread")
-        val systemContext = XposedHelpers.callMethod(currentThread, "getSystemContext") as Context
-        val versionName = systemContext.packageManager.getPackageInfo(pkgName, 0).versionName
-        setVersion(WechatVersion(pkgName, versionName))
-        getHooks(param.appInfo.uid)?.hook(param.classLoader)
+        if (param.packageName == "com.tencent.mm") {
+            setVersion(WechatVersion(param))
+            getHooks(param.appInfo.uid)?.hook(param.classLoader)
+        }
     }
 
     private fun getHooks(uid: Int): WechatRevokeHook? {
