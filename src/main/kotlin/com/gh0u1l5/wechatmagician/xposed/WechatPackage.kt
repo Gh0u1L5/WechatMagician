18c3c33214f1aa5dffd0ad26d8350e3cf0c03867
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-09-11 23:51:36 -0400

    Add depth in findClassesFromPackage()

--- src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/WechatPackage.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/WechatPackage.kt
@@ -43,8 +43,16 @@ class WechatPackage(param: XC_LoadPackage.LoadPackageParam) {
     }
 
     // findClassesFromPackage returns a list of all the classes contained in the given package
-    private fun findClassesFromPackage(apkFile: ApkFile, packageName: String): List<DexClass> {
-        return apkFile.dexClasses.filter { it.packageName == packageName }
+    private fun findClassesFromPackage(apkFile: ApkFile, packageName: String, depth: Int = 0): List<DexClass> {
+        return apkFile.dexClasses.filter predicate@ {
+            if (depth == 0) {
+               return@predicate it.packageName == packageName
+            }
+            val satisfyPrefix = it.packageName.startsWith(packageName)
+            val satisfyDepth =
+                    it.packageName.drop(packageName.length).count{it == '.'} == depth
+            return@predicate satisfyPrefix && satisfyDepth
+        }
     }
 
     // findClassWithMethod finds the class that have the given method from a list of classes.
