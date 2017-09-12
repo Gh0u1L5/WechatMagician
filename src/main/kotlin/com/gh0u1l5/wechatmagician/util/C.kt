72dadc1938fce8b10b3443d31e33f68d5c8c0fac
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-09-12 03:58:03 -0400

    Add object C as a shortcut for classes.

---
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/util/C.kt
@@ -0,0 +1,15 @@
+package com.gh0u1l5.wechatmagician.util
+
+import android.content.ContentValues
+
+object C {
+    val Boolean = Boolean::class.java
+    val ContentValues = ContentValues::class.java
+    val Int = Int::class.java
+    val Map = Map::class.java
+    val Object = Object::class.java
+    val String = String::class.java
+
+    val ObjectArray = Array<Any>::class.java
+    val StringArray = Array<String>::class.java
+}
