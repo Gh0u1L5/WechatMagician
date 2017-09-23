f17579fb0ee1d52ffb69dbb2de76da80196f9f32
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-09-23 05:53:33 -0400

    Add new object for the localized resources of the module.

---
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/ModuleResources.kt
@@ -0,0 +1,31 @@
+package com.gh0u1l5.wechatmagician.xposed
+
+import android.content.res.XModuleResources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.gh0u1l5.wechatmagician.R
+import java.io.InputStream
+
+// ModuleResources describes the localized resources used by the module.
+object ModuleResources {
+    lateinit var labelEasterEgg: String
+    lateinit var labelDeleted: String
+    lateinit var labelRecalled: String
+    lateinit var buttonSelectAll: String
+    lateinit var bitmapRecalled: Bitmap
+
+    fun init(res: XModuleResources) {
+        labelEasterEgg = res.getString(R.string.easter_egg)
+        labelDeleted = res.getString(R.string.label_deleted)
+        labelRecalled = res.getString(R.string.label_recalled)
+        buttonSelectAll = res.getString(R.string.button_select_all)
+
+        var imgStream: InputStream? = null
+        try {
+            imgStream = res.assets.open("image_recall_${res.getString(R.string.language)}.jpg")
+            bitmapRecalled = BitmapFactory.decodeStream(imgStream)
+        } finally {
+            imgStream?.close()
+        }
+    }
+}
