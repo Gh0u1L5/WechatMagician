74ea71af43e6ab32a0e4f694d80826c3addd5137
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-09-30 01:41:56 -0400

    Add new strings in ModuleResource.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/ModuleResources.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/xposed/ModuleResources.kt
@@ -8,15 +8,21 @@ import java.io.InputStream
 
 // ModuleResources describes the localized resources used by the module.
 object ModuleResources {
+    lateinit var menuSnsForward: String
+    lateinit var menuSnsScreenshot: String
     lateinit var labelEasterEgg: String
     lateinit var labelDeleted: String
     lateinit var buttonSelectAll: String
+    lateinit var promptScreenShot: String
     lateinit var bitmapRecalled: Bitmap
 
     fun init(res: XModuleResources) {
+        menuSnsForward = res.getString(R.string.menu_sns_forward)
+        menuSnsScreenshot = res.getString(R.string.menu_sns_screenshot)
         labelEasterEgg = res.getString(R.string.easter_egg)
         labelDeleted = res.getString(R.string.label_deleted)
         buttonSelectAll = res.getString(R.string.button_select_all)
+        promptScreenShot = res.getString(R.string.screenshot_prompt)
 
         var imgStream: InputStream? = null
         try {
