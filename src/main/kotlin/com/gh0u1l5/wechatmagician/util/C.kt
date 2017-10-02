c4d720692ee9b700386d8f1e35bf03620f44efd6
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-10-01 23:28:02 -0400

    Add new classes in C.kt

--- src/main/kotlin/com/gh0u1l5/wechatmagician/util/C.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/util/C.kt
@@ -10,12 +10,17 @@ object C {
     val Object = Object::class.java
     val String = String::class.java
 
+    val AttributeSet = android.util.AttributeSet::class.java
     val Bundle = android.os.Bundle::class.java
     val ContentValues = android.content.ContentValues::class.java
+    val Context = android.content.Context::class.java
     val Intent = android.content.Intent::class.java
     val ListView = android.widget.ListView::class.java
     val Menu = android.view.Menu::class.java
+    val MotionEvent = android.view.MotionEvent::class.java
+    val View = android.view.View::class.java
 
+    val ByteArray = ByteArray::class.java
     val ObjectArray = Array<Any>::class.java
     val StringArray = Array<String>::class.java
 }
