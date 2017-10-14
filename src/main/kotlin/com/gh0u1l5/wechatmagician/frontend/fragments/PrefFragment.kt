5119e161e6063510c7e2366e87fd22cf72e417df
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-10-13 23:31:52 -0400

    Fix the crash caused by detached fragment.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/fragments/PrefFragment.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/fragments/PrefFragment.kt
@@ -3,12 +3,28 @@ package com.gh0u1l5.wechatmagician.frontend.fragments
 import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.content.Context.MODE_WORLD_READABLE
+import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager.*
 import android.os.Bundle
 import android.preference.PreferenceFragment
 
-class PrefFragment : PreferenceFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
+class PrefFragment : PreferenceFragment() {
+
+    class OnSharedPreferenceChangeListener(private val context: Context): SharedPreferences.OnSharedPreferenceChangeListener {
+        override fun onSharedPreferenceChanged(preferences: SharedPreferences, key: String) {
+            if (key == "settings_interface_hide_icon") {
+                // Hide/Show the icon as required.
+                val hide = preferences.getBoolean(key, false)
+                val newState = if (hide) COMPONENT_ENABLED_STATE_DISABLED else COMPONENT_ENABLED_STATE_ENABLED
+                val pkg = context.packageName
+                val componentName = ComponentName(pkg, "$pkg.frontend.MainActivityAlias")
+                context.packageManager.setComponentEnabledSetting(
+                        componentName, newState, DONT_KILL_APP
+                )
+            }
+        }
+    }
 
     companion object {
         private val ARG_PREF_RES = "preferencesResId"
@@ -29,20 +45,11 @@ class PrefFragment : PreferenceFragment(), SharedPreferences.OnSharedPreferenceC
         if (arguments != null) {
             val preferencesResId = arguments.getInt(ARG_PREF_RES)
             addPreferencesFromResource(preferencesResId)
-            preferenceManager.sharedPreferences
-                    .registerOnSharedPreferenceChangeListener(this)
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(
+                    OnSharedPreferenceChangeListener(activity.applicationContext)
+            )
         }
     }
 
-    override fun onSharedPreferenceChanged(preferences: SharedPreferences, key: String) {
-        if (key == "settings_interface_hide_icon") {
-            // Hide/Show the icon as required.
-            val hide = preferences.getBoolean(key, false)
-            val newState = if (hide) COMPONENT_ENABLED_STATE_DISABLED else COMPONENT_ENABLED_STATE_ENABLED
-            val pkg = activity.packageName
-            val componentName = ComponentName(pkg, "$pkg.frontend.MainActivityAlias")
-            activity.packageManager.setComponentEnabledSetting(componentName, newState, DONT_KILL_APP)
-        }
-    }
 
 }
