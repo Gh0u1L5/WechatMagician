df3e5813bfd9fe3aff807ff3ab8765b593e30e10
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-10-14 16:07:40 -0400

    Change the background color of fragments.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/fragments/PrefFragment.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/fragments/PrefFragment.kt
@@ -5,8 +5,13 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager.*
+import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gh0u1l5.wechatmagician.R
 import java.io.File
 
 class PrefFragment : PreferenceFragment() {
@@ -37,6 +42,12 @@ class PrefFragment : PreferenceFragment() {
         }
     }
 
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return super.onCreateView(inflater, container, savedInstanceState).apply {
+            setBackgroundColor(getColor(R.color.card_background))
+        }
+    }
+
     // NOTE: On Android N, the support of MODE_WORLD_READABLE has been removed.
     //       So whenever the user exits from the PrefFragment we set file permissions manually.
     @SuppressLint("SetWorldReadable")
@@ -45,6 +56,14 @@ class PrefFragment : PreferenceFragment() {
         val prefDir = "${activity.applicationInfo.dataDir}/shared_prefs/"
         val filename = "${preferenceManager.sharedPreferencesName}.xml"
         File("$prefDir$filename").setReadable(true, false)
+    }
+
+    private fun getColor(resId: Int): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            resources.getColor(resId, context.theme)
+        } else {
+            resources.getColor(resId)
+        }
     }
 
     companion object {
