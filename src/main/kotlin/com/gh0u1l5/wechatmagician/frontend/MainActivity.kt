42a97da86d79e3eacdbbcaaab5032fca1f548555
Gh0u1L5 <Gh0u1L5@foxmail.com>
2017-10-29 13:54:43 -0400

    Get rid of WRITE_EXTERNAL_STORAGE permission.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/MainActivity.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.gh0u1l5.wechatmagician.frontend
 
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.app.Activity
 import android.app.Fragment
 import android.content.Intent
-import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.design.widget.NavigationView
@@ -36,21 +33,10 @@ class MainActivity : Activity(),
 
         nav_view.setNavigationItemSelectedListener(this)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(WRITE_EXTERNAL_STORAGE) != PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(WRITE_EXTERNAL_STORAGE), 0)
-            } else {
-                loadHomeFragment()
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults[0] != PERMISSION_GRANTED) {
-            finish()
-        }
-        loadHomeFragment()
+        findViewById<ConstraintLayout?>(R.id.main_container) ?: return
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_container, StatusFragment.newInstance())
+                .commit()
     }
 
     override fun onBackPressed() {
@@ -64,13 +50,6 @@ class MainActivity : Activity(),
     fun onGithubLinkClick(view: View?) {
         val url = Uri.parse(view?.context?.getString(R.string.view_about_project_github_url))
         view?.context?.startActivity(Intent(Intent.ACTION_VIEW).setData(url))
-    }
-
-    private fun loadHomeFragment() {
-        findViewById<ConstraintLayout?>(R.id.main_container) ?: return
-        fragmentManager.beginTransaction()
-                .replace(R.id.main_container, StatusFragment.newInstance())
-                .commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
