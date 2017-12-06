bb7ef06d12e6edcc6e679ed59176e154b20665c9
Emerson Lin <Em3rs0n@yahoo.com>
2017-12-05 20:52:50 -0500

    Add new constants for secret friends.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/Global.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/Global.kt
@@ -3,6 +3,9 @@ package com.gh0u1l5.wechatmagician
 object Global {
     val LOG_TAG = "WechatMagician"
 
+    val SALT = "W3ch4tM4g1c14n"
+    val SHA_MAGIC_STRING = "######SHA256######"
+
     val XPOSED_PACKAGE_NAME   = "de.robv.android.xposed.installer"
     val WECHAT_PACKAGE_NAME   = "com.tencent.mm"
     val MAGICIAN_PACKAGE_NAME = "com.gh0u1l5.wechatmagician"
@@ -16,6 +19,7 @@ object Global {
     val STATUS_FLAG_DATABASE    = "Database"
     val STATUS_FLAG_XML_PARSER  = "XMLParser"
     val STATUS_FLAG_URI_ROUTER  = "UriRouter"
+    val STATUS_FLAG_COMMAND     = "SearchBarCommand"
 
     val ACTION_UPDATE_PREF = "$MAGICIAN_PACKAGE_NAME.ACTION_UPDATE_PREF"
     val PREFERENCE_STRING_LIST_KEYS = listOf("settings_sns_keyword_blacklist_content")
