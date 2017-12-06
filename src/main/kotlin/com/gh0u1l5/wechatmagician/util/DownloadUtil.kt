c242ab012b4058c9113ac606ebe8f956a6f665ed
Emerson Lin <Em3rs0n@yahoo.com>
2017-12-05 21:05:34 -0500

    Fix wrong import.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/util/DownloadUtil.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/util/DownloadUtil.kt
@@ -1,7 +1,7 @@
 package com.gh0u1l5.wechatmagician.util
 
 import com.gh0u1l5.wechatmagician.backend.WechatPackage
-import com.gh0u1l5.wechatmagician.storage.SnsCache
+import com.gh0u1l5.wechatmagician.storage.cache.SnsCache
 import de.robv.android.xposed.XposedHelpers
 import java.net.URL
 
