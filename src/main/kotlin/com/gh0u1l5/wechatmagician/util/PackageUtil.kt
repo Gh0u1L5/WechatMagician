6ecb8aff984f31daa3965d8c618a567baf043ec7
Emerson Lin <Em3rs0n@yahoo.com>
2017-12-07 15:49:24 -0500

    Make changes in PackageUtil for Issue #22

--- src/main/kotlin/com/gh0u1l5/wechatmagician/util/PackageUtil.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/util/PackageUtil.kt
@@ -56,7 +56,7 @@ object PackageUtil {
     private val classesCache: MutableMap<Pair<String, Int>, Classes> = HashMap()
 
     // shadowCopy copy all the fields of the object obj into the object copy.
-    fun shadowCopy(obj: Any, copy: Any, clazz: Class<*>? = obj.javaClass) {
+    @JvmStatic fun shadowCopy(obj: Any, copy: Any, clazz: Class<*>? = obj.javaClass) {
         if (clazz == null) {
             return
         }
@@ -68,46 +68,57 @@ object PackageUtil {
     }
 
     // findClassIfExists looks up and returns a class if it exists, otherwise it returns null.
-    fun findClassIfExists(className:String, classLoader: ClassLoader?): Class<*>? =
+    @JvmStatic fun findClassIfExists(className:String, classLoader: ClassLoader?): Class<*>? =
             try { findClass(className, classLoader) } catch (_: Throwable) { null }
 
     // getClassName parses the standard class name of the given DexClass.
-    private fun getClassName(clazz: DexClass): String {
+    @JvmStatic fun getClassName(clazz: DexClass): String {
         return clazz.classType
                 .replace('/', '.') // replace delimiters
                 .drop(1) // drop leading 'L'
                 .dropLast(1) //drop trailing ';'
     }
 
+    // getPackageName parses the package name of the given DexClass.
+    // NOTE: Here we do not use DexClass.getPackageName because of Issue #22
+    // Reference: https://github.com/Gh0u1L5/WechatMagician/issues/22
+    @JvmStatic fun getPackageName(clazz: DexClass): String {
+        val className = getClassName(clazz)
+        val delimiter = className.lastIndexOf('.')
+        if (delimiter == -1) {
+            return ""
+        }
+        return className.substring(0, delimiter)
+    }
+
     // findClassesFromPackage returns a list of all the classes contained in the given package.
-    fun findClassesFromPackage(
+    @JvmStatic fun findClassesFromPackage(
             loader: ClassLoader, classes: Array<DexClass>, packageName: String, depth: Int = 0
     ): Classes {
         if ((packageName to depth) in classesCache) {
             return classesCache[packageName to depth]!!
         }
 
-        val result = Classes(classes.filter predicate@ {
+        classesCache[packageName to depth] = Classes(classes.filter predicate@ { clazz ->
+            val currentPackage = getPackageName(clazz)
             if (depth == 0) {
-                return@predicate it.packageName == packageName
+                return@predicate currentPackage == packageName
             }
-            val satisfyPrefix = it.packageName.startsWith(packageName)
-            val satisfyDepth =
-                    it.packageName.drop(packageName.length).count{it == '.'} == depth
+            val satisfyPrefix = currentPackage.startsWith(packageName)
+            val satisfyDepth = currentPackage.drop(packageName.length).count{it == '.'} == depth
             return@predicate satisfyPrefix && satisfyDepth
         }.mapNotNull { findClassIfExists(getClassName(it), loader) })
-        classesCache[packageName to depth] = result
-        return result
+        return classesCache[packageName to depth]!!
     }
 
     // findMethodExactIfExists looks up and returns a method if it exists, otherwise it returns null.
-    fun findMethodExactIfExists(
+    @JvmStatic fun findMethodExactIfExists(
             clazz: Class<*>?, methodName: String, vararg parameterTypes: Class<*>
     ): Method? =
             try { findMethodExact(clazz, methodName, *parameterTypes) } catch (_: Throwable) { null }
 
     // findMethodsByExactParameters returns a list of all methods declared/overridden in a class with the specified parameter types.
-    fun findMethodsByExactParameters(
+    @JvmStatic fun findMethodsByExactParameters(
             clazz: Class<*>?, returnType: Class<*>?, vararg parameterTypes: Class<*>
     ): List<Method> {
         if (clazz == null) {
@@ -119,20 +130,20 @@ object PackageUtil {
     }
 
     // findFieldsWithGenericType finds all the fields of the given type.
-    fun findFieldsWithType(clazz: Class<*>?, typeName: String): List<Field> {
+    @JvmStatic fun findFieldsWithType(clazz: Class<*>?, typeName: String): List<Field> {
         return clazz?.declaredFields?.filter {
             it.type.name == typeName
         } ?: listOf()
     }
 
     // findFieldsWithGenericType finds all the fields of the given generic type.
-    fun findFieldsWithGenericType(clazz: Class<*>?, genericTypeName: String): List<Field> {
+    @JvmStatic fun findFieldsWithGenericType(clazz: Class<*>?, genericTypeName: String): List<Field> {
         return clazz?.declaredFields?.filter {
             it.genericType.toString() == genericTypeName
         } ?: listOf()
     }
 
-    fun findAndHookMethod(clazz: Class<*>?, method: Method?, callback: XC_MethodHook) {
+    @JvmStatic fun findAndHookMethod(clazz: Class<*>?, method: Method?, callback: XC_MethodHook) {
         if (clazz == null) {
             log("findAndHookMethod: clazz should not be null")
             return
