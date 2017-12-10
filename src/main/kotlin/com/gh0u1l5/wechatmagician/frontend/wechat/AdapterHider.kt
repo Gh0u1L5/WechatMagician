353e55c8c0297a5b48df9c3f24e53c151e5a7cf1
Emerson Lin <Em3rs0n@yahoo.com>
2017-12-10 02:30:32 -0500

    Fix the wrong algorithm to map hidden items.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/wechat/AdapterHider.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/wechat/AdapterHider.kt
@@ -6,29 +6,57 @@ import java.util.concurrent.ConcurrentHashMap
 
 object AdapterHider {
 
-    // hideTable records the index of items we need to hide
-    private val hideTable: MutableMap<BaseAdapter, Set<Int>> = ConcurrentHashMap()
+    data class Section(
+            val start: Int,  // Inclusive
+            val end: Int,    // Exclusive
+            val base: Int
+    ) {
+        operator fun contains(index: Int) = (start <= index) && (index < end)
+
+        fun size() = end - start
+
+        fun split(index: Int): List<Section> {
+            val length = index - base
+            return listOf(
+                    Section(start, start + length, base),
+                    Section(start + length, end - 1, base + length + 1)
+            ).filter { it.size() != 0 }
+        }
+    }
+
+    // adapterMap records the sections of items we want to show
+    private val adapterMap: MutableMap<BaseAdapter, List<Section>> = ConcurrentHashMap()
 
     fun afterGetCount(param: XC_MethodHook.MethodHookParam) {
         val adapter = param.thisObject as BaseAdapter
-        val count = param.result as Int
-        val hideSize = hideTable[adapter]?.size ?: 0
-        param.result = count - hideSize
+        val sections = adapterMap[adapter] ?: return
+        if (sections.isNotEmpty()) {
+            param.result = sections.sumBy { it.size() }
+        }
     }
 
     fun beforeGetItem(param: XC_MethodHook.MethodHookParam) {
         val adapter = param.thisObject as BaseAdapter
         val index = param.args[0] as Int
-        val list = hideTable[adapter] ?: return
-        param.args[0] =  index + list.filter { it <= index }.size
+        val sections = adapterMap[adapter] ?: return
+        sections.forEach { section ->
+            if (index in section) {
+                param.args[0] = section.base + (index - section.start)
+                return
+            }
+        }
     }
 
     fun beforeNotifyDataSetChanged(param: XC_MethodHook.MethodHookParam, predicate: (Any?) -> Boolean) {
         val adapter = param.thisObject as BaseAdapter
-        hideTable[adapter] = setOf()
-        hideTable[adapter] = (0 until adapter.count).filter { index ->
+        adapterMap[adapter] = listOf()
+
+        val initial = listOf(Section(0, adapter.count, 0))
+        adapterMap[adapter] = (0 until adapter.count).filter { index ->
             predicate(adapter.getItem(index))
-        }.toSet()
+        }.fold(initial, { sections, index ->
+            sections.dropLast(1) + sections.last().split(index)
+        })
     }
 
     fun notifyAdapter(adapter: Any?) {
