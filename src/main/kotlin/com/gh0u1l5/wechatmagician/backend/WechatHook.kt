e888cbc68ed26600c7b51457f710d65b57e8e8f3
Emerson Lin <Em3rs0n@yahoo.com>
2017-12-19 15:26:58 -0500

    Remove useless codes.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/backend/WechatHook.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/backend/WechatHook.kt
@@ -1,7 +1,6 @@
 package com.gh0u1l5.wechatmagician.backend
 
 import android.content.Context
-import android.os.Build
 import com.gh0u1l5.wechatmagician.C
 import com.gh0u1l5.wechatmagician.Global.FOLDER_SHARED
 import com.gh0u1l5.wechatmagician.Global.MAGICIAN_PACKAGE_NAME
@@ -72,17 +71,6 @@ class WechatHook : IXposedHookLoadPackage {
         SecretFriendList.init(context)
         settings.load(context, PREFERENCE_NAME_SETTINGS)
         developer.load(context, PREFERENCE_NAME_DEVELOPER)
-
-        // Cannot use lazy evaluation on Android 7.0 for now.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            WechatPackage.javaClass.declaredFields.forEach {
-                try {
-                    (it.get(WechatPackage) as? Lazy<*>)?.value
-                } catch (_: Throwable) {
-                    // Ignore this one
-                }
-            }
-        }
 
         val pluginDeveloper = Developer
         pluginDeveloper.init(loader, developer)
