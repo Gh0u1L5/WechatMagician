df913b4423e0e5d9a31b1d3fa5c3f7abe8711702
Emerson Lin <Em3rs0n@yahoo.com>
2018-01-19 00:09:57 -0500

    Improve the logic about resource hooks.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/backend/WechatResHook.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/backend/WechatResHook.kt
@@ -2,7 +2,6 @@ package com.gh0u1l5.wechatmagician.backend
 
 import android.content.res.XModuleResources
 import com.gh0u1l5.wechatmagician.Global.STATUS_FLAG_RESOURCES
-import com.gh0u1l5.wechatmagician.Global.WECHAT_PACKAGE_NAME
 import de.robv.android.xposed.IXposedHookInitPackageResources
 import de.robv.android.xposed.IXposedHookZygoteInit
 import de.robv.android.xposed.XposedBridge.log
@@ -11,8 +10,8 @@ import de.robv.android.xposed.callbacks.XC_InitPackageResources
 class WechatResHook : IXposedHookZygoteInit, IXposedHookInitPackageResources {
 
     companion object {
-        private var MODULE_PATH: String? = null
-        var MODULE_RES: XModuleResources? = null
+        @Volatile private var MODULE_PATH: String? = null
+        @Volatile var MODULE_RES: XModuleResources? = null
     }
 
     override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
@@ -20,14 +19,13 @@ class WechatResHook : IXposedHookZygoteInit, IXposedHookInitPackageResources {
     }
 
     override fun handleInitPackageResources(resparam: XC_InitPackageResources.InitPackageResourcesParam) {
-        if (resparam.packageName != WECHAT_PACKAGE_NAME) {
-            return
-        }
-
-        // Load resources
         try {
-            MODULE_RES = XModuleResources.createInstance(MODULE_PATH, resparam.res)
-            WechatPackage.setStatus(STATUS_FLAG_RESOURCES, true)
-        } catch (e: Throwable) { log(e) }
+            if (MODULE_RES == null) {
+                MODULE_RES = XModuleResources.createInstance(MODULE_PATH, resparam.res)
+                WechatPackage.setStatus(STATUS_FLAG_RESOURCES, true)
+            }
+        } catch (t: Throwable) {
+            MODULE_RES = null; log(t)
+        }
     }
 }
