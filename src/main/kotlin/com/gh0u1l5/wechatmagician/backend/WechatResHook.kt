07dcc1f4274b3657da5da65e2990bd70bf9a6a68
Emerson Lin <Em3rs0n@yahoo.com>
2018-01-20 00:15:49 -0500

    Prevent making irrelevant resource hooks.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/backend/WechatResHook.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/backend/WechatResHook.kt
@@ -2,6 +2,7 @@ package com.gh0u1l5.wechatmagician.backend
 
 import android.content.res.XModuleResources
 import com.gh0u1l5.wechatmagician.Global.STATUS_FLAG_RESOURCES
+import com.gh0u1l5.wechatmagician.Global.WECHAT_PACKAGE_NAME
 import de.robv.android.xposed.IXposedHookInitPackageResources
 import de.robv.android.xposed.IXposedHookZygoteInit
 import de.robv.android.xposed.XposedBridge.log
@@ -19,6 +20,9 @@ class WechatResHook : IXposedHookZygoteInit, IXposedHookInitPackageResources {
     }
 
     override fun handleInitPackageResources(resparam: XC_InitPackageResources.InitPackageResourcesParam) {
+        if (resparam.packageName != WECHAT_PACKAGE_NAME) {
+            return
+        }
         try {
             if (MODULE_RES == null) {
                 MODULE_RES = XModuleResources.createInstance(MODULE_PATH, resparam.res)
