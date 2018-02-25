b103665a307bc855a42d28b0e214ee37522c1da1
Emerson Lin <Em3rs0n@yahoo.com>
2018-02-24 21:22:27 -0500

    Bump up gradle and remove multidex.

--- src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/WMApplication.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/frontend/WMApplication.kt
@@ -1,11 +1,11 @@
 package com.gh0u1l5.wechatmagician.frontend
 
+import android.app.Application
 import android.content.Context
 import android.content.res.Configuration
-import android.support.multidex.MultiDexApplication
 import com.gh0u1l5.wechatmagician.util.LocaleUtil
 
-class WMApplication: MultiDexApplication() {
+class WMApplication: Application() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(LocaleUtil.onAttach(base))
     }
